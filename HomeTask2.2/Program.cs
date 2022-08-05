4489a41a0224d225a6f7611945046e4348e54f79
nihadmm <nihadmm@code.edu.az>
2022-08-05 12:34:12 +0400

    HomeTask2.2

---
+++ HomeTask2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeTask2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi=3.14;
+            Console.WriteLine("Enter the radius: ");
+            string r=Console.ReadLine();
+            int radius=int.Parse(r);
+            Console.WriteLine("perimeter of circle: "+2*pi*radius);
+            Console.WriteLine("area of circle: "+pi*radius*radius);
+            
+        }
+    }
+}
